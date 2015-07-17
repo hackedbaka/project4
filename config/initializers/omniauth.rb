79dcff26959378c2fe4fdc28cfdb167a4a19fb2c
hackedbaka <jeffrey.wu23@gmail.com>
2015-07-16 17:45:43 -0700

    working code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :steam, ENV["STEAM_WEB_API_KEY"]
 
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-    access_type: 'offline',
-    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+   
+    redirect_uri:'http://robor.herokuapp.com/auth/google_oauth2/callback'
 	}
 end
