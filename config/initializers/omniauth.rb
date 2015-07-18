665b89579f3651a05a2cff63f5acbb3d54154be6
hackedbaka <jeffrey.wu23@gmail.com>
2015-07-18 01:05:50 -0700

    took out http:

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, ENV["STEAM_WEB_API_KEY"]
+  
 
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
    
