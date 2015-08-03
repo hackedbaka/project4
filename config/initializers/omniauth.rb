c9465296414d884e469801822f9ada546ac6e273
hackedbaka <jeffrey.wu23@gmail.com>
2015-08-03 06:15:42 -0700

    testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
    
-    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
-    # redirect_uri:'https://robor.herokuapp.com/auth/google_oauth2/callback'
+    # redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+    redirect_uri:'https://robor.herokuapp.com/auth/google_oauth2/callback'
 	}
 end
